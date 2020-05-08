2141104a73a5a6064daa172534cea36b14b741e0
Marcos Genesis <marcosgenesisof@gmail.com>
2020-05-07 22:40:44 -0300

    toasts-animations

--- frontend/src/components/ToastContainer/Toast/index.tsx
+++ frontend/src/components/ToastContainer/Toast/index.tsx
@@ -10,13 +10,14 @@ import { ToastMessage, useToast } from '../../../hooks/toast';
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 const icons = {
   info: <FiInfo size={24} />,
   error: <FiAlertCircle size={24} />,
   success: <FiCheckCircle size={24} />,
 };
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -27,7 +28,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [message.id, removeToast]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
       <div>
         <strong>{message.title}</strong>
--- frontend/src/components/ToastContainer/index.tsx
+++ frontend/src/components/ToastContainer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 import { Container } from './styles';
 import { ToastMessage } from '../../hooks/toast';
 import Toast from './Toast';
@@ -6,11 +7,22 @@ import Toast from './Toast';
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
-const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => (
-  <Container>
-    {messages.map(message => (
-      <Toast key={message.id} message={message} />
-    ))}
-  </Container>
-);
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+  );
+  return (
+    <Container>
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
+      ))}
+    </Container>
+  );
+};
 export default ToastContainer;
